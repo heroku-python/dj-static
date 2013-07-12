a7ba4d035ac22e7918c15e386e2dd46eb27204b1
Kenneth Reitz <me@kennethreitz.com>
2013-07-12 19:05:28 -0400

    metadata

--- README.rst
+++ README.rst
@@ -1,10 +1,20 @@
-Django-StaticServe
-==================
+DJ Static
+=========
 
-::
+This is a simple Django middleware utility that allows you to properly
+serve static assets from production with a WSGI server like Gunicorn.
+
+Usage
+-----
+
+Configure your static assets in ``settings.py``::
+
+   STATIC_ROOT = 'staticfiles'
+   STATIC_URL = '/static/'
+
+Then, update your ``wsgi.py`` file to use dj-static::
 
     from django.core.wsgi import get_wsgi_application
-    from django_staticserve import StaticFilesHandler
-    # from django.contrib.staticfiles.handlers import StaticFilesHandler
+    from dj_static import StaticCling
 
-    application = StaticFilesHandler(get_wsgi_application())
+    application = StaticCling(get_wsgi_application())
---
+++ setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+dj-database-url
+~~~~~~~~~~~~~~~
+
+.. image:: https://secure.travis-ci.org/kennethreitz/dj-database-url.png?branch=master
+
+This simple Django utility allows you to utilize the
+`12factor <http://www.12factor.net/backing-services>`_ inspired
+``DATABASE_URL`` environment variable to configure your Django application.
+
+
+Usage
+-----
+
+Configure your database in ``settings.py`` from ``DATABASE_URL``::
+
+    DATABASES = {'default': dj_database_url.config()}
+
+Parse an arbitrary Database URL::
+
+    DATABASES = {'default': dj_database_url.parse('postgres://...')}
+
+Supported databases
+-------------------
+
+Support currently exists for PostgreSQL, PostGIS, MySQL and SQLite.
+
+SQLite connects to file based databases. The same URL format is used, omitting
+the hostname, and using the "file" portion as the filename of the database.
+This has the effect of four slashes being present for an absolute file path:
+``sqlite:////full/path/to/your/database/file.sqlite``.
+
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='dj-database-url',
+    version='0.2.1',
+    url='https://github.com/kennethreitz/dj-database-url',
+    license='BSD',
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    description='Use Database URLs in your Django Application.',
+    long_description=__doc__,
+    py_modules=['dj_database_url'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
