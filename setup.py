1a3194558334c529a47f351b9554e9e172d89e78
Kenneth Reitz <me@kennethreitz.com>
2013-07-12 19:11:08 -0400

    v0.0.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name='dj-static',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/kennethreitz/dj-static',
     license='BSD',
     author='Kenneth Reitz',
